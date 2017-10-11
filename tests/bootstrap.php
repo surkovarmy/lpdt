0da276b1d8d7e9042ba74cf297d987635cd59aeb
gbublik <gbublik@gmail.com>
2017-10-12 00:16:54 +0400

    update package

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Разработано в комании Prodvigaeff.ru
+ * Date: 11.10.2017
+ * Time: 23:12
+ * @copyright Prodvigaeff.ru <info@prodvigaeff.ru>
+ * @author Большагин Вячеслав <gbublik@gmail.com>
+ * @version 1.0
+ */
